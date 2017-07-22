c8b2e0f3967f49f62d1ae42c75357ef9febba9b4
dot3v1L <cryptonly4you@gmail.com>
2017-07-22 17:42:50 +0200

    v1

---
+++ Skype Messenger v 0.4 by 3v1L XakFor/Avatar.cs
@@ -0,0 +1,34 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Threading;
+using SKYPE4COMLib;
+
+namespace Skype_Messenger_v_0._4_by_3v1L_XakFor
+{
+    internal class Avatar
+    {
+        private readonly Skype s;
+
+        public Avatar(Skype S)
+        {
+            s = S;
+        }
+
+        public Bitmap grabAvatarAPI(string handle)
+            //Saves users image from Skype using the API, then returns it as bitmap
+        {
+            var c = new Command();
+            File.Delete(Path.GetTempPath() + handle + ".png");
+            c.Command = string.Format("GET USER {0} AVATAR 1 {1}", handle, Path.GetTempPath() + handle + ".png");
+            s.SendCommand(c);
+            Thread.Sleep(100); //THIS IS MANDATORY
+            return new Bitmap(Path.GetTempPath() + handle + ".png");
+        }
+
+        public string grabAvatarOnline(string handle)
+            //Downloads it off Skypes website. You get the profile picture as if you aren't a contact. returns string
+        {
+            return "http://api.skype.com/users/" + handle + "/profile/avatar";
+        }
+    }
+}
---
+++ Skype Messenger v 0.4 by 3v1L XakFor/maintabm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Skype_Messenger_v_0._4_by_3v1L_XakFor
+{
+    public partial class maintabm : UserControl
+    {
+        public maintabm()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void bunifuFlatButton2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void bunifuFlatButton1_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
